808cde7d05c2a010b1f2368fa382561ff9035526
TANZIL <87266319+Tanjil2019@users.noreply.github.com>
2023-08-28 19:27:54 +0600

    temp add

---
+++ math.js
@@ -0,0 +1,32 @@
+// const result = Math.pow(3, 8);
+// console.log(result);
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+// console.log(gap);
+if(gap < 5){
+     // console.log('You guys can be Friends');
+}
+else{
+//      console.log('You guys can apart');
+}
+
+
+const number = 2.45443;
+const result2 = Math.ceil(number)
+// console.log(result2);
+
+
+// floor
+const result3 = Math.floor(number);
+// console.log(result3);
+
+
+// Random
+
+for(let i = 0; i < 20; i++){
+const result4 = Math.round(Math.random()*6);
+console.log(result4);
+}
